177fe6742e981f6e1fb1073c97873a157f22d286
lyyx5858 <lyyx5858@163.com>
2020-07-05 22:55:46 +0800

    Create struct-01.go
    creat struct-01.go

---
+++ struct-01.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+type s1 struct {   //结构体
+	age int
+}
+
+type s3 struct {  //嵌套结构体
+	s1			//等效为：t1 s1,此处省略了t1
+	s2 [3]s1  //结构体数组
+	name string
+}
+
+func main() {
+
+	a1 := s1{18}
+
+	a2 := [3]s1{s1{11},
+		a1,
+		s1{20}}
+
+	a3:=s3{s1{30},a2,"liuyan"}
+
+	fmt.Println(a1)
+	fmt.Printf("%T,%v", a2, a2)
+	fmt.Println()
+	fmt.Println(a3)
+
+	fmt.Println(a3.s1.age)
+
+
+
+}
