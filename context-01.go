17e94426b340cf52533b7edc81db62a81bbe62d7
Sikeer <sikeer@yahoo.com>
2021-04-27 22:57:14 +0800

    Create context-01.go

---
+++ context-01.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+"context"
+"fmt"
+"time"
+)
+
+func main() {
+	ctx := context.Background()
+
+	ctx, cancel := context.WithCancel(ctx)
+	go f(ctx)
+	time.Sleep(time.Second * 3)
+	cancel()
+	time.Sleep(time.Second * 1)
+}
+
+func f(ctx context.Context) {
+	i := 1
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("DONE")
+			fmt.Println(ctx.Err())
+			return
+		default:
+			fmt.Println(i)
+			time.Sleep(time.Second * 1)
+			i++
+		}
+	}
+}
+
+
