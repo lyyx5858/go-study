e4824fb46d064e60be54153249a1977e4dfc1a42
Sikeer <sikeer@yahoo.com>
2021-04-29 17:55:47 +0800

    add quic.go

--- Interface/interface-02.go
+++ Interface/interface-02.go
@@ -5,25 +5,72 @@ import (
 )
 
 type Liu int //Liu 是个本地定义的数据类型，其实就是int的别名
+type Wang int
 
-type interface1 interface { //所有实现了方法method1的变量都可以用interface1接口。
-	method1()
+type interface1 interface { //所有实现了方法method1和2的变量都可以用interface1接口。
+	method1()				//注意，必须实现所有的方法,只实现一个不行。
+	method2()
 }
+
+type struct1 struct{
+	i1 interface1
+	interface2
+
+}
+
+type interface2 interface {
+	method1()
+	method2()
+	method3()
+}
+
 
 func main() {
 
-	var l1 Liu //l1是数据类型Liu
-	test(l1)   //由于类型LIU的数据类型已经实现了method1，因此所有数据类型LIU都可以用interface1接口来调用test1函数。
+	var l1 Liu //l1是数据类型Liu的实例
+	test(l1)   //由于类型Liu的数据类型已经实现了method1，因此所有数据类型Liu都可以用interface1接口来调用test1函数
 	fmt.Printf("%T %v %v \n", l1, l1, &l1)
+
+	var w1 Wang
+	test(w1)   //注意：test函数的入参数应该是interface1类型, 但此处调用的是w1有三个方法,但是由于w1实现了inteface1的所有方法
+				//因此也可以直接调用。
+
+				//一个函数的输入参数是个接口的情况下，如果一个实例，只要它实现了这个接口的所有方法，它就可以被输入。
+				//举例：interface1有两个方法，inteface2有三个方法，所有以interface1的函数都可以用interface2.
+				//但用interface2的函数不用用inteface1,因为它少了一个方法。
+
+	var s1 struct1
+	s1.i1=l1
+	s1.interface2=w1
+
+	test(s1)
+
+
 }
 
 func (l Liu) method1() {
-	fmt.Println("this is method", l)
+	fmt.Println("this is method1", l)
 }
+func (l Liu) method2() {
+	fmt.Println("this is method2", l)
+}
+
+func (w Wang) method1() {
+	fmt.Println("W: this is method1", w)
+}
+func (w Wang) method2() {
+	fmt.Println("W: this is method2", w)
+}
+func (w Wang) method3() {
+	fmt.Println("W: this is method3", w)
+}
+
+
 
 func test(i interface1) { // 函数test的形参是接口 i，i的数据类型是interface1，而interface1的接口定义中：必须实现方法method1：见程序开并头！因此，所有实现了method1的数据类型都可调用函数test1.
 
 	i.method1() //接口i直接调用方法method1
+	i.method2()
 
 }
 
---
+++ net/quic.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"context"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"github.com/lucas-clemente/quic-go"
+	"math/big"
+	"time"
+)
+
+const addr = "localhost:4242"
+
+const clientMessage = "Hello"
+const serverMessage = "Hi"
+
+func main() {
+	go func() {
+		err := server()
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+
+	err := client()
+	if err != nil {
+		panic(err)
+	}
+
+	// 等待main和go程 执行完,防止server执行完自动结束
+	time.Sleep(time.Second * 5)
+
+	fmt.Println("=============")
+	fmt.Println("Main is Over")
+}
+
+// 客户端
+func client() error {
+	tlsConf := &tls.Config{
+		InsecureSkipVerify: true,
+		NextProtos:         []string{"quic-echo-example"},
+	}
+
+	session, err := quic.DialAddr(addr, tlsConf, nil)
+	if err != nil {
+		return err
+	}
+
+	stream, err := session.OpenStreamSync(context.Background())
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Client: Sending '%s'\n", clientMessage)
+	_, err = stream.Write([]byte(clientMessage))
+	if err != nil {
+		return err
+	}
+
+	buf := make([]byte, 128)
+	_, err = stream.Read(buf)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Client: Got '%s'\n", buf)
+
+	return nil
+}
+
+// 服务端
+func server() error {
+	listener, err := quic.ListenAddr(addr, generateTLSConfig(), nil)
+	if err != nil {
+		return err
+	}
+	sess, err := listener.Accept(context.Background())
+	if err != nil {
+		return err
+	}
+	stream, err := sess.AcceptStream(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+	buf := make([]byte, 128)
+	_, err = stream.Read(buf)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Server: Got '%s'\n", buf)
+
+	fmt.Printf("Server: Sending '%s'\n", serverMessage)
+	_, err = stream.Write([]byte(serverMessage))
+	return err
+}
+
+func generateTLSConfig() *tls.Config {
+	key, err := rsa.GenerateKey(rand.Reader, 1024)
+	if err != nil {
+		panic(err)
+	}
+	template := x509.Certificate{SerialNumber: big.NewInt(1)}
+	certDER, err := x509.CreateCertificate(rand.Reader, &template, &template, &key.PublicKey, key)
+	if err != nil {
+		panic(err)
+	}
+	keyPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(key)})
+	certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certDER})
+
+	tlsCert, err := tls.X509KeyPair(certPEM, keyPEM)
+	if err != nil {
+		panic(err)
+	}
+	return &tls.Config{
+		Certificates: []tls.Certificate{tlsCert},
+		NextProtos:   []string{"quic-echo-example"},
+	}
+}
