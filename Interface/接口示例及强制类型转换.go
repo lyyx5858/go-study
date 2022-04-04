df33fcb7f1f06f177a07f24981809c804f9df2b4
sikeer <lyyx5858@163.com>
2022-04-04 09:28:29 +0800

    add 断言

--- Interface/接口示例及强制类型转换.go
+++ Interface/接口示例及强制类型转换.go
@@ -14,6 +14,9 @@ type interface1 interface { //所有实现了方法method1和method2的变量都
 
 func test(i interface1) { //i的数据类型是interface1，
 	fmt.Println(i)
+	x, ok := i.(Wang) //接口断言，意思就是：判断一下，传进来的接口的类型是不是"wang"?
+	fmt.Println(x, ok)
+
 	i.method1() //接口i直接调用方法method1
 	i.method2()
 }
@@ -25,10 +28,11 @@ func main() {
 	var x1 int
 
 	x1 = 1
-	w1 = "好好学习"
 	l1 = Liu(x1) //当底层数据类型一样时，可以进行强制类型转换。因为x1和l1都是int.
-
 	test(l1)
+	test(Liu(100))
+
+	w1 = "好好学习"
 	test(w1)
 
 }
